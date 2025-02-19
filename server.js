daa64ef65ef69f29020c4de9d5cf577db1a8aeca
João Pedro Gundim Guimarães <joaopedrogundimg@gmail.com>
2025-02-18 20:26:25 -0400

    final,acho

--- server.js
+++ server.js
@@ -3,44 +3,52 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors({
-  origin: 'https://clubpenguin-1.onrender.com',
-}));
-app.use(express.json());
+    origin: 'https://clubpenguin-1.onrender.com'
+  }));
+  app.use(express.json());
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-app.post('/enviar-email', async (req, res) => {
-  const { nomePinguim, email, confirmacao } = req.body;
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: 'Confirmação de Presença',
-    text: `Olá Pinguim: ${nomePinguim}, sua confirmação de presença foi registrada como "${confirmacao}". Obrigado!`,
-  };
+  app.post('/enviar-email', async (req, res) => {
+    const { nomePinguim, email, confirmacao } = req.body;
+  
+    
+    const mailOptions = {
+      from: 'joaopedrogundimg@gmail.com',
+      to: email,
+      subject: 'Confirmação de Presença',
+      html: `
+        <h1>BEM VINDO(A) AO CLUB PENGUIN</h1>
+        <p>Olá Pinguim: ${nomePinguim}, sua confirmação de presença foi registrada como "${confirmacao}". Obrigado!</p>
+        <p>Todos os direitos reservados ao Joyttape Studios c.a</p>
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'E-mail enviado com sucesso!' });
-  } catch (error) {
-    console.error('Erro ao enviar e-mail:', error);
-    res.status(500).json({ message: 'Erro ao enviar e-mail.' });
-  }
-});
+        <img src="./email.jpg" alt="Imagem de boas-vindas" style="width: 300px; height: auto;">
+      `,
+    };
+  
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'E-mail enviado com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao enviar e-mail:', error);
+      res.status(500).json({ message: 'Erro ao enviar e-mail.' });
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
--- src/confirmacao.js
+++ src/confirmacao.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import './App.css';
+
+import cor1 from './cor1.png';  
+import cor2 from './cor2.png';
+import cor3 from './cor3.png';
+import cor4 from './cor4.png';
+import cor5 from './cor5.png';
+import cor6 from './cor6.png';
 
 const ConfirmacaoPresenca = () => {
   const [nomePinguim, setNomePinguim] = useState('');
   const [email, setEmail] = useState('');
   const [confirmacao, setConfirmacao] = useState('');
   const [mensagem, setMensagem] = useState('');
-
+  const [imagemLado, setImagemLado] = useState(cor1); 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (nomePinguim && email && confirmacao) {
@@ -19,7 +28,7 @@ const ConfirmacaoPresenca = () => {
         });
 
         if (response.ok) {
-          setMensagem(`Pinguim ${nomePinguim} ativado com sucesso! E-mail enviado para ${email}.`);
+          setMensagem(`Ebaa Pinguim ${nomePinguim} ativado com sucesso! E-mail enviado para ${email}.`);
         } else {
           setMensagem('Erro ao enviar o e-mail. Tente novamente.');
         }
@@ -32,51 +41,121 @@ const ConfirmacaoPresenca = () => {
     }
   };
 
+  const mudarCorPersonagem = (cor) => {
+    switch (cor) {
+      case 'cor1':
+        setImagemLado(cor1);
+        break;
+      case 'cor2':
+        setImagemLado(cor2);
+        break;
+      case 'cor3':
+        setImagemLado(cor3);
+        break;
+      case 'cor4':
+        setImagemLado(cor4);
+        break;
+      case 'cor5':
+        setImagemLado(cor5);
+        break;
+      case 'cor6':
+        setImagemLado(cor6);
+        break;
+      default:
+        setImagemLado(cor1);
+    }
+  };
+
   return (
-    <div>
-      <h1>Confirmação de Presença</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Nome do Pinguim:
-          <input
-            type="text"
-            value={nomePinguim}
-            onChange={(e) => setNomePinguim(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          E-mail:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Confirmar Presença:
-          <label>
-            <input
-              type="radio"
-              name="confirmacao"
-              value="Sim"
-              onChange={(e) => setConfirmacao(e.target.value)}
-            /> Sim
+    <div className="fundobranco">
+      <div className="conteudo">
+        <div className="imagem-lado">
+          <img src={imagemLado} alt="Imagem do Personagem" />
+        </div>
+        <div className="formulario-lado">
+          <h1 className="titulo">Confirmação de Presença</h1>
+          <label className="aviso">
+            Atenção: Por favor não convide outras pessoas sem minha permissão!
           </label>
-          <label>
-            <input
-              type="radio"
-              name="confirmacao"
-              value="Não"
-              onChange={(e) => setConfirmacao(e.target.value)}
-            /> Não
-          </label>
-        </label>
-        <br />
-        <button type="submit">Enviar</button>
-      </form>
-      {mensagem && <p>{mensagem}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="campo-formulario">
+              <label className="jogo">Nome do Pinguim:</label>
+              <input
+                type="text"
+                value={nomePinguim}
+                onChange={(e) => setNomePinguim(e.target.value)}
+              />
+            </div>
+            <div className="campo-formulario">
+              <label className="jogo">E-mail:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <p>Selecione uma cor: </p>
+            <div className="cores">
+              <button
+                className="cor1"
+                type="button"
+                onClick={() => mudarCorPersonagem('cor1')}
+              ></button>
+              <button
+                className="cor2"
+                type="button"
+                onClick={() => mudarCorPersonagem('cor2')}
+              ></button>
+              <button
+                className="cor3"
+                type="button"
+                onClick={() => mudarCorPersonagem('cor3')}
+              ></button>
+              <button
+                className="cor4"
+                type="button"
+                onClick={() => mudarCorPersonagem('cor4')}
+              ></button>
+              <button
+                className="cor5"
+                type="button"
+                onClick={() => mudarCorPersonagem('cor5')}
+              ></button>
+              <button
+                className="cor6"
+                type="button"
+                onClick={() => mudarCorPersonagem('cor6')}
+              ></button>
+            </div>
+
+            <div className="campo-formulario">
+              <label className="jogo">Confirmar Presença:</label>
+              <div className="radio-group">
+                <label className="jogo">
+                  <input
+                    type="radio"
+                    name="confirmacao"
+                    value="Sim"
+                    onChange={(e) => setConfirmacao(e.target.value)}
+                  />{" "}
+                  Sim
+                </label>
+                <label className="jogo">
+                  <input
+                    type="radio"
+                    name="confirmacao"
+                    value="Não"
+                    onChange={(e) => setConfirmacao(e.target.value)}
+                  />{" "}
+                  Não
+                </label>
+              </div>
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
+          {mensagem && <p>{mensagem}</p>}
+        </div>
+      </div>
     </div>
   );
 };
